f2b9c11b9c1a9ac6274297bd319b79c232ac594d
Jungkon Kim <primer79@gmail.com>
2021-07-20 07:22:06 +0900

    first codecommit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import Amplify, { Analytics } from 'aws-amplify'
+//import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import AppProvider from './context/AppProvider'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+import App from './App'
+import Product from './pages/Product'
+import Checkout from './pages/Checkout'
+import PlacedOrder from './pages/PlacedOrder'
+
+import awsconfig from './aws-exports'
+Amplify.configure(awsconfig)
+
+Analytics.record('Store_Load')
+
+Analytics.autoTrack('pageView', {
+	enable: true,
+	type: 'SPA',
+})
+
+const routing = (
+	<AppProvider>
+		<Router>
+			<Switch>
+				<Route exact path="/" component={App} />
+				<Route path="/product/:id" component={Product} />
+				<Route path="/checkout" component={Checkout} />
+				<Route path="/ordercomplete" component={PlacedOrder} />
+				<Route component={App} />
+			</Switch>
+		</Router>
+	</AppProvider>
+)
+
+ReactDOM.render(routing, document.getElementById('root'))
+
+//serviceWorker.unregister();
