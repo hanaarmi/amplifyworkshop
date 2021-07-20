bb08522109006a7a95ca261752589b829c9182a7
Jungkon <jungkon@amazon.com>
2021-07-20 14:10:16 +0900

    dynamodb table name in labmda function is modified

--- amplify/backend/function/newamplifyworkshoplambda/src/app.js
+++ amplify/backend/function/newamplifyworkshoplambda/src/app.js
@@ -23,7 +23,7 @@ AWS.config.update({ region: process.env.TABLE_REGION })
 
 const dynamodb = new AWS.DynamoDB.DocumentClient()
 
-let tableName = 'newamplifyworkshopitems'
+let tableName = 'newamplifyworkshopitems-dev'
 if (process.env.ENV && process.env.ENV !== 'NONE') {
 	tableName = tableName + '-' + process.env.ENV
 }
